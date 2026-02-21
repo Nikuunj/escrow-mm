44b8a665b89982195093dd742c752f4d3f3e1359
Nikunj <mnikunj2622@gmail.com>
2026-02-21 11:08:50 +0530

    fix: init_if_neeeded and errors mod

--- programs/escrow-mm/src/lib.rs
+++ programs/escrow-mm/src/lib.rs
@@ -2,9 +2,13 @@ use anchor_lang::prelude::*;
 
 declare_id!("FcuXNkCebST8UeqnEekXUvUS4gGq3AiqpPpbbsSrXDgu");
 
+pub mod accounts;
+pub mod errors;
 pub mod instructions;
 pub mod states;
 
+pub use accounts::*;
+pub use errors::*;
 pub use instructions::*;
 pub use states::*;
 
