c805b861d14cb0b31af902c0077ffa841341b699
Nikunj <mnikunj2622@gmail.com>
2026-02-24 00:28:38 +0530

    update: derive account for take_fud

---
+++ programs/escrow-mm/src/accounts/take_fund.rs
@@ -0,0 +1,66 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+use crate::Escrow;
+
+#[derive(Accounts)]
+pub struct TakeFund<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    pub maker: SystemAccount<'info>,
+
+    #[account(mint::token_program = token_program_a)]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(mint::token_program = token_program_b)]
+    pub mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        close = maker,
+        has_one = maker,
+        has_one = mint_a,
+        has_one = mint_b,
+        seeds = [b"escrow", maker.key().as_ref(), mint_a.key().as_ref()],
+        bump = escrow.bump
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority =  maker,
+        associated_token::token_program = token_program_b
+    )]
+    pub maker_ata_b: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority =  escrow,
+        associated_token::token_program = token_program_a
+    )]
+    pub escrow_ata_a: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority =  taker,
+        associated_token::token_program = token_program_a
+    )]
+    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority =  taker,
+        associated_token::token_program = token_program_b
+    )]
+    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program_a: Interface<'info, TokenInterface>,
+    pub token_program_b: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
