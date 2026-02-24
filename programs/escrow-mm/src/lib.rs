35cd133d368c5be8eb7e2b265c54b1df2d0d9396
Nikunj <mnikunj2622@gmail.com>
2026-02-25 00:27:34 +0530

    update: mod and lib to get attach

--- programs/escrow-mm/src/lib.rs
+++ programs/escrow-mm/src/lib.rs
@@ -15,11 +15,25 @@ pub use states::*;
 #[program]
 pub mod escrow_mm {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+
+    pub fn deposit_fund(
+        ctx: Context<DepositFund>,
+        deposit_amount: u64,
+        require_fund: u64,
+        seeds: u64,
+    ) -> Result<()> {
+        ctx.accounts.deposit_fund(deposit_amount)?;
+        ctx.accounts
+            .create_vault_profile(seeds, require_fund, &ctx.bumps)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund()?;
+        ctx.accounts.close_accounts()
+    }
+
+    pub fn swap_fund(ctx: Context<TakeFund>) -> Result<()> {
+        ctx.accounts.take_fund()?;
+        ctx.accounts.close_accounts()
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
