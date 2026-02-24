d0ef41faf2094df79a4c168f26adc7de0551b8e3
Nikunj <mnikunj2622@gmail.com>
2026-02-24 11:34:44 +0530

    implement: buyfund derive_account

---
+++ programs/escrow-mm/src/derive_accounts/buy_fund.rs
@@ -0,0 +1,27 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+#[derive(Accounts)]
+pub struct BuyFund<'info> {
+    #[account(mut)]
+    pub buyer: Signer<'info>,
+
+    #[account(mint::token_program = token_program_a)]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority =  buyer,
+        associated_token::token_program = token_program_a
+
+    )]
+    pub buyer_ata_a: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program_a: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
